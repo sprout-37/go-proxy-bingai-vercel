36c1dd831d0d6c5513f14fd4fa3d03f8d2c572ab
sprout-37 <77878579+sprout-37@users.noreply.github.com>
2023-05-25 11:14:41 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ api/index.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"adams549659584/go-proxy-bingai/api/helper"
+	"adams549659584/go-proxy-bingai/common"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		http.Redirect(w, r, common.PROXY_WEB_PAGE_PATH, http.StatusFound)
+	} else {
+		if !helper.CheckAuth(r) {
+			helper.UnauthorizedResult(w)
+			return
+		}
+		common.NewSingleHostReverseProxy(common.BING_URL).ServeHTTP(w, r)
+	}
+}
